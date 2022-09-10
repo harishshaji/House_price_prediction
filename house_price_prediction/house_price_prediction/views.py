e123e7a02aae17d7410b09ed79ab5cf56c0907a7
Harish shaji <93399652+harishshaji@users.noreply.github.com>
2022-09-10 10:25:46 +0530

    User interface created

---
+++ house_price_prediction/house_price_prediction/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+def predict(request):
+    return render(request, "predict.html")
+
+
+def result(request):
+    return render(request, "predict.html")
+
